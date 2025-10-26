8e088a25f182a35a6d7a3c69084ad8e3f2e7e5d1
Jack <90474780+jto-d@users.noreply.github.com>
2025-10-26 17:20:02 -0400

    lightweight server

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+current_activity = {"state": "idle"}
+
+@app.route('/activity', methods=['GET'])
+def get_activity():
+  return jsonify(current_activity)
+
+@app.route('/activity', methods=['POST'])
+def set_activity():
+  data = request.get_json()
+  if "state" in data:
+    current_activity["state"] = data["state"]
+  return jsonify(success=True)
+
+if __name__ == '__main__':
+  app.run(debug=True)
