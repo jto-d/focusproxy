bb7ae846e547da7a2ef064e8fe9e871438389c0d
Jack <90474780+jto-d@users.noreply.github.com>
2025-10-27 15:07:46 -0400

    testing railway deploy

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -19,4 +20,5 @@ def set_activity():
   return jsonify(success=True)
 
 if __name__ == '__main__':
-  app.run(debug=True, port=8080)
+  port = int(os.environ.get('PORT', 8080))
+  app.run(host='0.0.0.0', port=port, debug=False)
